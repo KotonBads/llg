88dd0910cdb5e7dd2fc68141e988b44a71059e3e
KotonBads <kotonbads@duck.com>
2023-10-30 23:57:52 +0800

    setup

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/KotonBads/llg-launcher/launcher
+
+go 1.20
+
+require github.com/KotonBads/llgutils v1.1.0
---
+++ launcher.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	utils "github.com/KotonBads/llgutils"
+)
+
+func main() {
+	// logging
+	timestamp := time.Now().Format("2006-01-02 15:04:05")
+
+	file, err := utils.CreateLog(fmt.Sprintf("launcherlogs/%s.log", timestamp))
+
+	if err != nil {
+		log.Printf("[WARN] Could not create a log file: %s", err)
+	}
+
+	log.SetOutput(file)
+
+	launchbody := utils.LaunchBody{
+		OS:      "linux",
+		Arch:    "x64",
+		Version: "1.8.9",
+		Module:  "forge",
+	}
+	launchmeta, _ := utils.FetchLaunchMeta(launchbody)
+	launchmeta.DownloadArtifacts("temp")
+	launchmeta.DownloadCosmetics("temp/textures")
+}
